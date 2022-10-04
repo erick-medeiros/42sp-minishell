4f907f769d26723898937500540dbb9690ea8cc7
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-04 10:43:41 -0300

    feat: add test function here_doc

--- tests/unit/prompt.c
+++ tests/unit/prompt.c
@@ -1,5 +1,6 @@
 #include "Unity/unity.h"
 #include "minishell.h"
+#include <string.h>
 #include <unistd.h>
 
 void test_function_command_is_equal(void) {
@@ -31,8 +32,54 @@ void test_function_get_content_fd(void) {
 	free(content);
 }
 
+void test_function_here_doc(void) {
+	int pipefd_expected[2];
+	int pipefd_std[2];
+	char *content;
+	char *expected;
+	pid_t pid;
+	int fd;
+	int fd_devnull;
+
+	expected = "oi";
+	fd_devnull = open("/dev/null", O_WRONLY);
+	if (pipe(pipefd_std) == -1)
+		TEST_IGNORE_MESSAGE("Error pipe");
+	if (pipe(pipefd_expected) == -1)
+		TEST_IGNORE_MESSAGE("Error pipe");
+	pid = fork();
+	if (pid < 0)
+		TEST_IGNORE_MESSAGE("Error fork");
+	if (pid == 0) {
+		close(pipefd_std[1]);
+		close(pipefd_expected[0]);
+		dup2(pipefd_std[0], STDIN);
+		dup2(fd_devnull, STDOUT);
+		dup2(fd_devnull, STDERR);
+		close(pipefd_std[0]);
+		close(fd_devnull);
+		fd = here_doc("EOF");
+		content = get_content_fd(fd);
+		write(pipefd_expected[1], content, strlen(content));
+		exit(0);
+	} else {
+		close(fd_devnull);
+		close(pipefd_std[0]);
+		close(pipefd_expected[1]);
+		write(pipefd_std[1], expected, strlen(expected));
+		write(pipefd_std[1], "\n", 1);
+		write(pipefd_std[1], "EOF", 3);
+		close(pipefd_std[1]);
+		wait(NULL);
+		content = get_content_fd(pipefd_expected[0]);
+		TEST_ASSERT_EQUAL_STRING(expected, content);
+		close(pipefd_expected[0]);
+	}
+}
+
 void file_prompt(void) {
 	RUN_TEST(test_function_command_is_equal);
 	RUN_TEST(test_function_command_ends_with);
 	RUN_TEST(test_function_get_content_fd);
+	RUN_TEST(test_function_here_doc);
 }
