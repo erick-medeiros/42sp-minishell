39de118b5733b420a877d66ff76bab3308835010
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-20 11:10:10 -0300

    tests: add executor (get_paths, get_pathname)

--- tests/unit/main_test.c
+++ tests/unit/main_test.c
@@ -1,19 +1,42 @@
 #include "Unity/unity.h"
+#include <string.h>
 
 void file_list_test(void);
 void file_prompt_test(void);
 void file_builtins_test(void);
 void file_environment_test(void);
+void file_executor_test(void);
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-int main() {
-	UNITY_BEGIN();
+void run_all_test() {
 	file_prompt_test();
 	file_list_test();
 	file_builtins_test();
 	file_environment_test();
+	file_executor_test();
+}
+
+int main(int argc, char *argv[]) {
+	UNITY_BEGIN();
+
+	if (argc == 1)
+		run_all_test();
+	else {
+		char *file = argv[1];
+		if (strcmp("list", file) == 0)
+			file_list_test();
+		else if (strcmp("prompt", file) == 0)
+			file_prompt_test();
+		else if (strcmp("builtins", file) == 0)
+			file_builtins_test();
+		else if (strcmp("environment", file) == 0)
+			file_environment_test();
+		else if (strcmp("executor", file) == 0)
+			file_executor_test();
+	}
+
 	return UNITY_END();
 }
