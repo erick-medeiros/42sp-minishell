ef2d12edeacb79307ec4164583e18b20abefa1db
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-20 11:07:31 -0300

    feat: add free_string_list

--- src/free/free.c
+++ src/free/free.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 11:38:19 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/19 18:28:48 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/10/20 11:01:31 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,16 @@ void	free_minishell(t_minishell *minishell)
 {
 	clear_list(minishell->pipelines, free_pipeline);
 	clear_list(minishell->token_list, free_token);
+}
+
+void	free_string_list(char **str)
+{
+	size_t	i;
+
+	if (!str)
+		return ;
+	i = -1;
+	while (str[++i])
+		free(str[i]);
+	free(str);
 }
