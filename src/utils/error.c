21b2799f8c4c26e85ef95d3567ed9d93668b4dac
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-22 15:32:45 -0300

    refactor: add newline in panic_error

--- src/utils/error.c
+++ src/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 13:01:34 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/22 13:16:07 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/10/22 14:25:20 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	panic_error(char *msg)
 {
 	write(STDERR, msg, ft_strlen(msg));
+	write(STDERR, "\n", 1);
 	exit(1);
 }
