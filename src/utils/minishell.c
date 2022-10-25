3c2b4412d2262ba4b7b442d8c0696ce08a0fd4f3
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-25 19:02:00 -0300

    refactor: init free and destroy of minishell

--- src/utils/minishell.c
+++ src/utils/minishell.c
@@ -6,15 +6,43 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 17:19:38 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/25 17:28:09 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/10/25 18:50:53 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parser_internals.h"
+
+void	free_token(void *content);
 
 void	init_minishell(t_minishell *minishell, char **envp)
 {
-	minishell->path_list = get_paths(envp);
-	envp_to_list(envp, &minishell->env_list);
-	minishell->envp = envp;
+	minishell->token_list = NULL;
+	minishell->pipelines = NULL;
+	minishell->envp = NULL;
+	minishell->path_list = NULL;
+	minishell->env_list.len = 0;
+	minishell->env_list.list = NULL;
+	if (envp)
+	{
+		minishell->path_list = get_paths(envp);
+		envp_to_list(envp, &minishell->env_list);
+		minishell->envp = envp;
+	}
+}
+
+void	free_minishell(t_minishell *minishell)
+{
+	if (minishell->pipelines)
+		clear_list(minishell->pipelines, (void (*)(void *))destroy_pipeline);
+	if (minishell->token_list)
+		clear_list(minishell->token_list, free_token);
+}
+
+void	destroy_minishell(t_minishell *minishell)
+{
+	free_minishell(minishell);
+	if (minishell->env_list.list)
+		clear_list(minishell->env_list.list, del_var_node);
+	free_string_list(minishell->path_list);
 }
