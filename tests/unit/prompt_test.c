b712790f9acc9bfa74251ccad4c5a00df1a8c88e
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-26 16:14:28 -0300

    tests: relocated tests

--- tests/unit/prompt_test.c
+++ tests/unit/prompt_test.c
@@ -4,35 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-void test_function_command_is_equal(void) {
-	TEST_ASSERT_TRUE(command_is_equal("exit", "exit"));
-	TEST_ASSERT_FALSE(command_is_equal("exita", "exit"));
-	TEST_ASSERT_FALSE(command_is_equal("exi", "exit"));
-}
-
-void test_function_command_ends_with(void) {
-	TEST_ASSERT_TRUE(command_ends_with("cmd |", '|'));
-	TEST_ASSERT_TRUE(command_ends_with("|", '|'));
-	TEST_ASSERT_FALSE(command_ends_with("cmd", '|'));
-	TEST_ASSERT_FALSE(command_ends_with("", '|'));
-	TEST_ASSERT_FALSE(command_ends_with(NULL, '|'));
-}
-
-void test_function_get_content_fd(void) {
-	const char *expected = "test";
-	char *content;
-	int pipefd[2];
-
-	if (pipe(pipefd) == -1)
-		TEST_IGNORE_MESSAGE("Error pipe");
-	write(pipefd[1], expected, strlen(expected));
-	close(pipefd[1]);
-	content = get_content_fd(pipefd[0]);
-	close(pipefd[0]);
-	TEST_ASSERT_EQUAL_STRING(expected, content);
-	free(content);
-}
-
 void test_here_doc(void) {
 	int pipefd[2];
 	char *expected;
@@ -114,9 +85,6 @@ void test_ends_in_pipe(void) {
 
 int file_prompt_test(void) {
 	UNITY_BEGIN();
-	RUN_TEST(test_function_command_is_equal);
-	RUN_TEST(test_function_command_ends_with);
-	RUN_TEST(test_function_get_content_fd);
 	RUN_TEST(test_here_doc);
 	RUN_TEST(test_ends_in_pipe);
 	return UNITY_END();
--- tests/unit/utils_test.c
+++ tests/unit/utils_test.c
@@ -46,6 +46,35 @@ void test_function_new_pipeline() {
 	destroy_pipeline(pipeline);
 }
 
+void test_function_command_is_equal(void) {
+	TEST_ASSERT_TRUE(command_is_equal("exit", "exit"));
+	TEST_ASSERT_FALSE(command_is_equal("exita", "exit"));
+	TEST_ASSERT_FALSE(command_is_equal("exi", "exit"));
+}
+
+void test_function_command_ends_with(void) {
+	TEST_ASSERT_TRUE(command_ends_with("cmd |", '|'));
+	TEST_ASSERT_TRUE(command_ends_with("|", '|'));
+	TEST_ASSERT_FALSE(command_ends_with("cmd", '|'));
+	TEST_ASSERT_FALSE(command_ends_with("", '|'));
+	TEST_ASSERT_FALSE(command_ends_with(NULL, '|'));
+}
+
+void test_function_get_content_fd(void) {
+	const char *expected = "test";
+	char *content;
+	int pipefd[2];
+
+	if (pipe(pipefd) == -1)
+		TEST_IGNORE_MESSAGE("Error pipe");
+	write(pipefd[1], expected, strlen(expected));
+	close(pipefd[1]);
+	content = get_content_fd(pipefd[0]);
+	close(pipefd[0]);
+	TEST_ASSERT_EQUAL_STRING(expected, content);
+	free(content);
+}
+
 int test_group_new(void) {
 	UNITY_BEGIN();
 	RUN_TEST(test_function_init_minishell);
@@ -55,9 +84,13 @@ int test_group_new(void) {
 }
 
 int file_utils_test(void) {
-	int err;
-	err = test_group_new();
-	if (!err)
-		return (err);
-	return (0);
+	int status;
+	status = test_group_new();
+	if (status)
+		return (status);
+	UNITY_BEGIN();
+	RUN_TEST(test_function_command_is_equal);
+	RUN_TEST(test_function_command_ends_with);
+	RUN_TEST(test_function_get_content_fd);
+	return (UNITY_END());
 }
