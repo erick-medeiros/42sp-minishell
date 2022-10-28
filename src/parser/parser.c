be8a603aa24716be23fd1277034b05b498da52c5
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-28 15:06:52 -0300

    feat: add open_fd

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 10:12:35 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/28 13:43:23 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/10/28 15:04:35 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 #include "structs.h"
 #include "parser_internals.h"
 
+typedef enum e_mode {
+	IN_MODE,
+	OUT_MODE,
+	APPEND_MODE,
+}	t_mode;
+
 void	parser_add_arg_cmd(t_cmd *cmd, char *arg);
+int		open_fd(char *pathname, int mode);
 
 void	parser(t_minishell *minishell)
 {
@@ -111,4 +118,21 @@ void	parser_add_arg_cmd(t_cmd *cmd, char *arg)
 	cmd->argv[i] = ft_strdup(arg);
 	cmd->argv[cmd->argc] = NULL;
 	free_string_list(temp);
+}
+
+int	open_fd(char *pathname, int mode)
+{
+	int	fd;
+	int	permissions;
+
+	permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if (mode == IN_MODE)
+		fd = open(pathname, O_RDONLY);
+	else if (mode == OUT_MODE)
+		fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	else if (mode == APPEND_MODE)
+		fd = open(pathname, O_WRONLY | O_CREAT | O_APPEND, permissions);
+	else
+		fd = open(pathname, O_RDWR | O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	return (fd);
 }
