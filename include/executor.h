484d252b8e4721327579cb4d245ed7cc18bceaf0
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-10-31 19:34:08 -0300

    refactor: relocated structs

--- include/executor.h
+++ include/executor.h
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 19:14:10 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/31 13:18:56 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/10/31 15:30:15 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	pipeline_executor(t_minishell *minishell, t_pipeline *pipeline);
 void	run_command(t_minishell *minishell, t_pipeline *pipeline,
 			t_cmd *command);
 void	command_exit_status(t_cmd *command);
+void	builtins(t_minishell *minishell, t_cmd *command);
 
 // Pipeline
 
@@ -36,18 +37,6 @@ void	child_process(t_minishell *minishell, t_cmd *command);
 void	update_io(t_minishell *minishell, t_cmd *command);
 void	exit_process(t_minishell *minishell, int status);
 void	process_exit_status(t_cmd *command);
-
-typedef enum e_tree_type {
-	TREE_TYPE_CMD,
-	TREE_TYPE_PIPE
-}		t_tree_type;
-
-typedef struct s_tree {
-	t_tree_type		type;
-	void			*content;
-	struct s_tree	*left;
-	struct s_tree	*right;
-}		t_tree;
 
 t_tree	*convert_list_to_tree(t_pipeline *pipeline);
 void	tree_executor(t_minishell *minishell, t_pipeline *pipeline);
