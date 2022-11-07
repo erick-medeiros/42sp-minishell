4ee217448fb876f4d067894a4bb36cc0d3e15f8a
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-11-07 20:37:06 -0300

    feat: updated OLDPWD

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,26 +6,34 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 11:53:49 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/10/28 19:47:27 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/11/07 20:32:18 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
 #include "expansor.h"
+#include "structs.h"
 
 //cd — change the working directory
 
 void	builtin_cd(char *path, t_vlst *vars)
 {
 	char	*retptr;
+	t_node	*node;
 
 	chdir(path);
 	retptr = get_pwd();
 	if (retptr)
 	{
+		node = find_node_by_content(vars->list, "PWD", find_env_var);
 		if (vars)
+		{
+			if (node)
+				update_var(vars, new_var_node_from_name_val("OLDPWD",
+						((t_var *)node->content)->val));
 			update_var(vars, new_var_node_from_name_val("PWD", retptr));
+		}
 		free(retptr);
 	}
 }
