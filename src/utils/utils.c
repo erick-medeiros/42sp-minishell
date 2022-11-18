40294945d8203fddd306081b37583fbc95ae3faa
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-11-18 15:37:52 -0300

    refactor: change location functions

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 14:38:59 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/11/15 08:56:22 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/11/18 15:35:25 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,29 @@ int	ft_streq(char *str1, char *str2)
 	if (ft_strncmp(str1, str2, len) == 0)
 		return (TRUE);
 	return (FALSE);
+}
+
+void	panic_error(char *msg)
+{
+	char	*err;
+
+	err = "Panic Error: ";
+	write(STDERR, err, ft_strlen(err));
+	write(STDERR, msg, ft_strlen(msg));
+	write(STDERR, "\n", 1);
+}
+
+int	error_message(int status, char *fields[])
+{
+	if (!fields)
+		return (status);
+	ft_putstr_fd("minishell", STDERR);
+	while (*fields)
+	{
+		ft_putstr_fd(": ", STDERR);
+		ft_putstr_fd(*fields, STDERR);
+		++fields;
+	}
+	ft_putstr_fd("\n", STDERR);
+	return (status);
 }
