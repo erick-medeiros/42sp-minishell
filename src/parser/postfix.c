176da83e4a5a97495772fc50877bbfd09e9e63ab
Gustavo Machado Coelho <gmachado@student.42sp.org.br>
2022-11-28 03:29:12 -0300

    feat: implement operator precedence

--- src/parser/postfix.c
+++ src/parser/postfix.c
@@ -3,32 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   postfix.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 02:42:05 by gmachado          #+#    #+#             */
-/*   Updated: 2022/11/25 11:25:27 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/11/27 06:28:09 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser.h"
+#include "structs.h"
 
 static int	push_postfix_parenthesis(t_node **opstack, t_queue *cmds,
 				t_tree *tree);
 static int	transfer_node(t_node **src, t_queue *dst);
+static int	precedence(t_tree_type t);
 
 int	push_postfix(t_node **opstack, t_queue *cmds, t_tree *tree)
 {
+	t_tree	*stack_top;
+
 	if (tree->type == TREE_TYPE_OPAR || tree->type == TREE_TYPE_CPAR)
 		return (push_postfix_parenthesis(opstack, cmds, tree));
 	if (is_op(tree->type))
 	{
-		while (*opstack != NULL
-			&& ((t_tree*)(*opstack)->content)->type != TREE_TYPE_OPAR
-			&& ((t_tree*)(*opstack)->content)->type != TREE_TYPE_CPAR)
+		while (*opstack != NULL)
 		{
-			if (transfer_node(opstack, cmds))
-				return (ERR_ALLOC);
+			stack_top = (t_tree *)((*opstack)->content);
+			if (stack_top->type != TREE_TYPE_OPAR
+				&& (precedence(tree->type) <= precedence(stack_top->type)))
+			{
+				if (transfer_node(opstack, cmds))
+					return (ERR_ALLOC);
+			}
 		}
 		return (push_node(opstack, tree));
 	}
@@ -54,7 +61,7 @@ static int	push_postfix_parenthesis(t_node **opstack, t_queue *cmds,
 			*opstack = remove_node(*opstack, del_cmd_tree_node);
 		return (OK);
 	}
-	return (1);
+	return (ERR_BAD_SYNTAX);
 }
 
 int	flush_postfix(t_node **tmp_stack, t_queue *cmds)
@@ -79,4 +86,9 @@ static int	transfer_node(t_node **src, t_queue *dst)
 	result = enqueue(dst, popped->content);
 	free(popped);
 	return (result);
+}
+
+static int	precedence(t_tree_type t)
+{
+	return (t == TREE_TYPE_PIPE);
 }
