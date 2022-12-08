90ccc0b28694fb0f55079646864490bb2c4262bc
Gustavo Machado Coelho <gmachado@student.42sp.org.br>
2022-12-08 08:59:13 -0300

    fix: fix errno check in command_redirect()

--- src/executor/redirect.c
+++ src/executor/redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:41:39 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/12/07 22:58:46 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/12/08 08:56:43 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,21 @@ int	command_redirect(t_cmd *cmd, t_vlst *env)
 	if (cmd->status != OK)
 		return (cmd->status);
 	node = cmd->redirect;
-	errno = 0;
 	while (node)
 	{
 		token = node->content;
 		if (token->type == TOKEN_INPUT || token->type == TOKEN_HEREDOC)
+		{
 			cmd->input = open_redir(token, cmd->input);
+			if (cmd->input == -1 && errno)
+				return (error_message2(1, token->value, strerror(errno)));
+		}
 		else if (token->type == TOKEN_OUTPUT || token->type == TOKEN_APPEND)
+		{
 			cmd->output = open_redir(token, cmd->output);
-		if (errno)
-			return (error_message2(1, token->value, strerror(errno)));
+			if (cmd->output == -1 && errno)
+				return (error_message2(1, token->value, strerror(errno)));
+		}
 		node = node->next;
 	}
 	return (OK);
